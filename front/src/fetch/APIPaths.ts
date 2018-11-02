39c8576ed8b123d1f3ef9400da3de1bf32093d54
Egorov Sergey <Sergey.Egorov@noveogroup.com>
2018-11-02 19:53:07 +0200

    fix path

--- front/src/fetch/APIPaths.ts
+++ front/src/fetch/APIPaths.ts
@@ -45,5 +45,5 @@ export enum APIPath {
     STUDIO                                  = '/studio',
     STUDIO_FIRST_LETTERS                    = '/studio/first-letters',
     CHANNEL                                 = '/channel',
-    CUSTOMER_NEW                            = '/customer-new '
+    CUSTOMER_NEW                            = '/customer-new'
 }
