22f1960532a565fd2e89fa04b34f249d9997b510
Egorov Sergey <Sergey.Egorov@noveogroup.com>
2018-11-23 00:45:36 +0200

    use duration inputs for manual type

--- front/src/components/Calendar/DurationPicker.tsx
+++ front/src/components/Calendar/DurationPicker.tsx
@@ -1,16 +1,13 @@
 import * as React from 'react';
 import * as classNames from 'classnames';
+import * as styles from './DurationPicker.scss';
 import padStart from 'lodash-es/padStart';
 import { observer } from 'mobx-react';
-import { computed } from 'mobx';
+import { action, computed, observable } from 'mobx';
 import { Button } from '../Button';
 import { IconArrowTopBlue } from '../Icons';
 import { TimeIncrements } from 'store';
 
-// Styles
-const s = require('./DurationPicker.css');
-
-// Props
 interface DurationPickerProps {
     onChange?: (totalMinutes: number) => void;
     className?: string | null;
@@ -19,7 +16,6 @@ interface DurationPickerProps {
     totalMinutesValue: number;
 }
 
-// Component
 @observer
 export class DurationPicker extends React.Component<DurationPickerProps, {}> {
     static get defaultProps(): DurationPickerProps {
@@ -31,6 +27,8 @@ export class DurationPicker extends React.Component<DurationPickerProps, {}> {
             totalMinutesValue: 60,
         };
     }
+
+    @observable private isHoursActive: boolean = false;
 
     @computed
     private get hours(): number {
@@ -56,33 +54,100 @@ export class DurationPicker extends React.Component<DurationPickerProps, {}> {
                 {this.props.label && <p className="durationPickerLabel">{this.props.label}</p>}
 
                 <Button
-                    className={s.decreaseArrow}
+                    className={styles.decreaseArrow}
                     onClick={this.handleDurationChange(false)}
                     icon={{
                         size: 'nopadding',
                         background: 'none',
-                        element: <IconArrowTopBlue width={15} height={20} transform={'rotateZ(180deg)'} />,
+                        element: <IconArrowTopBlue width={15} height={20} transform={'rotateZ(180deg)'}/>,
                     }}
                 />
 
                 <p className="durationPickerTime">
-                    <strong>{this.parsedHoursAndMinutes.hours}</strong>
+                    <strong>
+                        <input
+                            type="number"
+                            value={this.getHoursValue()}
+                            onChange={this.handleDurationHoursManualChange}
+                            onClick={this.selectInputValue}
+                            className={styles.manualTypeInput}
+                        />
+                    </strong>
                     <span>H</span>
-                    <strong>{this.parsedHoursAndMinutes.minutes}</strong>
+                    <strong>
+                        <input
+                            type="number"
+                            value={this.parsedHoursAndMinutes.minutes}
+                            onChange={this.handleDurationMinutesManualChange}
+                            onClick={this.selectInputValue}
+                            onBlur={this.handleMinutesManualInput}
+                            className={styles.manualTypeInput}
+                        />
+                    </strong>
                     <span>M</span>
                 </p>
 
                 <Button
-                    className={s.increaseArrow}
+                    className={styles.increaseArrow}
                     onClick={this.handleDurationChange(true)}
                     icon={{
                         size: 'nopadding',
                         background: 'none',
-                        element: <IconArrowTopBlue width={15} height={20} />,
+                        element: <IconArrowTopBlue width={15} height={20}/>,
                     }}
                 />
             </div>
         );
+    }
+
+    @action
+    private handleDurationHoursManualChange = (event) => {
+        const currentValue: number = event.target.value;
+
+        if (currentValue >= 0 && currentValue <= 12) {
+            const newTotalMinutes: number = (currentValue * 60) + Number(this.minutes);
+
+            if (this.props.onChange) {
+                this.props.onChange(Number(newTotalMinutes));
+            }
+        }
+    };
+
+    @action
+    private handleDurationMinutesManualChange = (event) => {
+        this.setMinutes(event.target.value);
+    };
+
+    private getHoursValue(): string {
+        if (this.isHoursActive) {
+            return this.hours.toString();
+        } else {
+            return this.parsedHoursAndMinutes.hours.toString();
+        }
+    }
+
+    private selectInputValue = (event) => {
+        event.target.select();
+    };
+
+    private handleMinutesManualInput = () => {
+        if (this.minutes <= 10) {
+            this.setMinutes(0);
+        } else if (this.minutes !== 15 && this.minutes <= 17) {
+            this.setMinutes(15);
+        } else if (this.minutes !== 30 && this.minutes <= 35) {
+            this.setMinutes(30);
+        } else {
+            this.setMinutes(45);
+        }
+    };
+
+    private setMinutes(minutes: number) {
+        const newTotalMinutes: number = (this.hours * 60) + Number(minutes);
+
+        if (this.props.onChange && minutes <= 45) {
+            this.props.onChange(Number(newTotalMinutes));
+        }
     }
 
     private handleDurationChange = (increase: boolean) => () => {
@@ -106,7 +171,7 @@ export class DurationPicker extends React.Component<DurationPickerProps, {}> {
             newTotalMinutes = 0;
         }
 
-        if (this.props.onChange) {
+        if (this.props.onChange && newTotalMinutes <= (12 * 60 + 45)) {
             this.props.onChange(newTotalMinutes);
         }
     };
