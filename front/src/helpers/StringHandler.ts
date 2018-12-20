812a2e221e13f975d814d9e0d3320cb40e90fa1f
Grzegorz VIDUA Widła <greg@vidua.pl>
2018-12-20 20:00:56 +0100

    Added two helper functions

--- front/src/helpers/StringHandler.ts
+++ front/src/helpers/StringHandler.ts
@@ -2,4 +2,26 @@ export class StringHandler {
     public static capitalizeAllWordsInPhrase = (phrase: string): string => {
         return phrase.trim().replace(/\b\w/g, firstLetter => firstLetter.toUpperCase());
     };
+
+    public static joinWordsExcludingNulls = (
+        words: Array<string | number | null | undefined>,
+        separator: string
+    ): string => {
+        return words
+            .map(word => (typeof word === 'number' ? word.toString() : typeof word === 'string' ? word : null))
+            .filter(word => typeof word === 'string' && word !== null && word !== '')
+            .join(separator);
+    };
+
+    public static constructUserName = (
+        username: string,
+        userFirstName: string | null | undefined,
+        userLastName: string | null | undefined
+    ): string => {
+        if (userFirstName || userLastName) {
+            return StringHandler.joinWordsExcludingNulls([userFirstName, userLastName], ' ');
+        }
+
+        return username;
+    };
 }
