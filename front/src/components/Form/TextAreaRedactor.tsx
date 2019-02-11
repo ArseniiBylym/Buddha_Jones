bddfc6c9357f149e959e8d0556a249cd0e624b48
Alex K <agent150891@gmail.com>
2019-02-11 15:53:58 +0200

    Remove desc from header

--- front/src/components/Form/TextAreaRedactor.tsx
+++ front/src/components/Form/TextAreaRedactor.tsx
@@ -11,8 +11,32 @@ import './TextAreaRedactor.css';
 export class TextAreaRedactor extends React.Component<any, {}> {
 
     state = {
-        editorValue: EditorState.createEmpty()
+        editorValue: EditorState.createEmpty(),
+        editorHTML: this.props.value,
     };
+
+    addCopyPasteHandler = () => {
+        const area = document.getElementById('copyPasteField');
+        if (!area) {
+            return;
+        }
+        area.onpaste = (event: any) => {
+            var items = event.clipboardData.items;
+            for (let index in items) {
+                if (index) {
+                    var item = items[index];
+                    if (item.kind === 'file') {
+                        var blob = item.getAsFile();
+                        var reader = new FileReader();
+                        reader.onload = (e: any) => {
+                            this.updateTextField(e.target.result);
+                        };
+                        reader.readAsDataURL(blob);
+                    }
+                }
+            } 
+        };
+    }
 
     componentDidMount = () => {
         const contentBlock = htmlToDraft(this.props.value);
@@ -23,6 +47,24 @@ export class TextAreaRedactor extends React.Component<any, {}> {
                 editorValue: editorState,
             });
         }
+
+        setTimeout(() => {
+            this.addCopyPasteHandler();
+        }, 500);
+    }
+
+    updateTextField = (imgData) => {
+        let newTextareaValue = this.state.editorHTML.slice();
+        newTextareaValue = newTextareaValue + `<img width="300" height="auto" src="${imgData}" alt="">`;
+        const contentBlock = htmlToDraft(newTextareaValue);
+        if (contentBlock) {
+            const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
+            const editorState = EditorState.createWithContent(contentState);
+            this.setState({
+                editorValue: editorState,
+            });
+            this.props.onChange(newTextareaValue);
+        }
     }
 
     onEditorStateChange = (editor) => {
@@ -31,7 +73,14 @@ export class TextAreaRedactor extends React.Component<any, {}> {
             editorValue: editor,
             editorHTML: editorHTML 
         });
-        this.props.onChange(editorHTML);
+        const emptyTagIndex = editorHTML.indexOf('<p></p>');
+        let cleanedHTML = '';
+        if (emptyTagIndex !== -1) {
+            cleanedHTML = editorHTML.split('<p></p>').join('');
+        } else {
+            cleanedHTML = editorHTML;
+        }
+        this.props.onChange(cleanedHTML);
     }
 
     // uploadImageCallBack = (file) => {
@@ -39,7 +88,7 @@ export class TextAreaRedactor extends React.Component<any, {}> {
 
     render() {
         return (
-            <div className="my-class">
+            <div id="copyPasteField">
                 <Editor
                     editorState={this.state.editorValue}
                     onEditorStateChange={this.onEditorStateChange}
@@ -47,8 +96,8 @@ export class TextAreaRedactor extends React.Component<any, {}> {
                     editorClassName="demo-editor"
                     toolbarClassName="demo-toolbar"
                     // toolbar={{
-                    //     image: { uploadCallback: this.uploadImageCallBack, alt: { present: true, mandatory: true } },
-                    // }}
+                        //     image: { uploadCallback: this.uploadImageCallBack, alt: { present: true, mandatory: true } },
+                        // }}
                 />
             </div>
         );
